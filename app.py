a36e4b3108efef836523ba34295f7546d62f0cac
priyanshup3138q-art <priyanshup3138q@gmail.com>
2026-04-08 17:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -657,9 +657,9 @@ def seed():
 with app.app_context():
     db.create_all()
     seed()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
